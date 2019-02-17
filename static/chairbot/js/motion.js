ce5caaac0ad750ef1d005981724310680a6d7a74
CHARISMA Lab <charisma@engr.oregonstate.edu>
2019-02-17 07:48:50 -0800

    Abrar: We are able to send string motion commands to packet_tester.py on reaspberryPi chairbot03

--- static/chairbot/js/motion.js
+++ static/chairbot/js/motion.js
@@ -22,21 +22,22 @@ var chairTwist04 = new ROSLIB.Topic({
 var requestMotion03 = new ROSLIB.Topic({
 	ros : ros,
 	name: '/requestMotion03',
-	messageType : 'std_msgs/Int8' 
+	messageType : 'std_msgs/String' 
 });
 
 var requestStop03 = new ROSLIB.Topic({
 	ros : ros,
 	name: '/requestStop03',
-	messageType : 'std_msgs/Int8' 
+	messageType : 'std_msgs/String' 
 });
 
 // Abrar: I'm using numbers 1= FORWARD, 3= BACKWARD, 2= RIGHT, 4= LEFT and 0=Stop
-const FORWARD = 1;
-const BACKWARD = 3;
-const RIGHT = 2;
-const LEFT= 4;
-const STOP = 0;
+
+const FORWARD = 'FORWARD'
+const BACKWARD = 'BACKWARD'
+const RIGHT = 'RIGHT'
+const LEFT=  'LEFT'
+const STOP = 'STOP'
 
 const ROTATE90POSITIVE = [
 	{linear: {x: 0.0, y:0.0, z:0.0}, angular: {x: 0.0, y:0.0, z:50.0}},
@@ -56,21 +57,26 @@ function askToRunSequence(motion) {
 	console.log("The requested motion is:" + motion);
 	//encode the packet to make it processesable by ROS
 
-	if (motion == 0) { //this is the STOP command
-		requestStopPacket = new ROSLIB.Message(motion)
-		console.log(requestStopPacket)
+	if (motion == STOP) { //this is the STOP command
+		requestStopPacket = new ROSLIB.Message({data: motion})
+		console.log("Stop Packet is " + requestStopPacket)
 		//send it to the right chair's topic
 		requestStop03.publish(requestStopPacket)
-		console.log("Stop Packet is " + requestStopPacket)
 	}
-	else if (motion > 0) { //this is one of the MOTION commands
-		requestMotionPacket = new ROSLIB.Message(motion)
+	else { //this is one of the MOTION commands
+		requestMotionPacket = new ROSLIB.Message({data: motion})
 		console.log("Motion Packet is " + requestMotionPacket)
 		//send it to the right chair's topic
 		requestMotion03.publish(requestMotionPacket);
 	}
 }
 
+function printData(message) {
+	console.log("Received msg which said " + message.data);
+}
+
+requestMotion03.subscribe(printData)
+requestStop03.subscribe(printData)
 
 function forward() {
 	askToRunSequence(FORWARD);
